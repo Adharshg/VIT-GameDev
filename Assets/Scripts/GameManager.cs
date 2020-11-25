c5b4d5d91bbb48cb35513e0e5935d90107d7cc7e
AdharshG <34604785+Adharshg@users.noreply.github.com>
2020-11-25 15:53:20 +0530

    Level 1 done
    Made a small UI and completed obj of level 1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	public Text display;
+
+	private string grid = "";
+	private string bowler = "";
+
+     // Start is called before the first frame update
+    void Start()
+    {
+        display.text = "Delivery type";
+    }
+
+    void Update(){
+    	if (grid != "" && bowler != ""){
+    		display.text = bowler + " grid " + grid;
+    	}
+    }
+
+    // Update is called once per frame
+    public void SelectGrid(Button button1){
+        grid = "" + button1.name;
+    }
+
+    public void SelectBowler(Button button2){
+    	bowler = "" + button2.name;
+
+    }
+}
