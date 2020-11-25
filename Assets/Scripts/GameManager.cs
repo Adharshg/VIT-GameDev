3f0601f0c966f77e56964aa7006843dc6f9cf4ea
AdharshG <34604785+Adharshg@users.noreply.github.com>
2020-11-25 16:55:26 +0530

    Level 2 done
    Displaying runs based of random gen.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,29 +6,124 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
 	public Text display;
+	public Text Run;
+	public GameObject FirstPanel;
+	public GameObject SecondPanel;
 
 	private string grid = "";
 	private string bowler = "";
+	private string delivery;
 
      // Start is called before the first frame update
     void Start()
     {
+    	FirstPanel.SetActive(true);
+    	SecondPanel.SetActive(false);
         display.text = "Delivery type";
     }
 
     void Update(){
     	if (grid != "" && bowler != ""){
-    		display.text = bowler + " grid " + grid;
+    		Batting();
     	}
     }
 
     // Update is called once per frame
-    public void SelectGrid(Button button1){
-        grid = "" + button1.name;
+    public void SelectGrid(Button GridButton){
+        grid = "" + GridButton.name;
     }
 
-    public void SelectBowler(Button button2){
-    	bowler = "" + button2.name;
+    public void SelectBowler(Button BowlerButton){
+    	bowler = "" + BowlerButton.name;
 
+    }
+
+    public void Batting(){
+    	delivery = bowler + " grid " + grid;
+    	display.text = delivery;
+    	bowler = "";
+    	grid = "";
+    	FirstPanel.SetActive(false);
+    	SecondPanel.SetActive(true);
+    }
+
+    public void RunSelect(Button RunButton){
+
+    	bool missed = false;
+    	int run = int.Parse(RunButton.name);
+    	float chance = UnityEngine.Random.Range(0f,1f);
+    	
+    	switch(run){
+    		case 0:
+    			if (chance < 0.9f){
+    				Run.text = "0 runs";
+    			}
+    			else{
+    				missed = true;
+    			}
+    			break;
+
+    		case 1:
+    			if (chance < 0.9f){
+    				Run.text = "1 runs";
+    			}
+    			else{
+    				missed = true;
+    			}
+    			break;
+
+    		case 2:
+    			if (chance < 0.85f){
+    				Run.text = "2 runs";
+    			}
+    			else{
+    				missed = true;
+    			}
+    			break;
+
+    		case 3:
+    			if (chance < 0.6f){
+    				Run.text = "3 runs";
+    			}
+    			else{
+    				missed = true;
+    			}
+    			break;
+
+    		case 4:
+    			if (chance < 0.35f){
+    				Run.text = "4 runs!";
+    			}
+    			else{
+    				missed = true;
+    			}
+    			break;
+
+    		case 6:
+    			if (chance < 0.2f){
+    				Run.text = "6 runs!!";
+    			}
+    			else{
+    				missed = true;
+    			}
+    			break;
+    	}
+    	if (missed){
+    		float missedOrOut = UnityEngine.Random.Range(0f,1f);
+    		if (missedOrOut > 0.5f){
+    			Run.text = "Missed";
+    		}
+    		else{
+    			Run.text = "Out!";
+    		}
+    	}
+    	Bowling();
+
+    }
+
+    public void Bowling(){
+    	display.text = "Delivery type";
+    	SecondPanel.SetActive(false);
+    	FirstPanel.SetActive(true);
     }
 }
